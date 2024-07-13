88f4efa3a4149ec4629d05e9b785ae47f1e4d6f8
S.G <shaulgav@gmail.com>
2024-07-13 09:32:57 +0200

    Deleted the assets folder and changed the index.js to main.js

--- assets/js/vars.js
+++
@@ -1,8 +0,0 @@
-export const body = document.body;
-export const themeToggleBtn = document.querySelector("#themeToggleBtn");
-export const weekdaysEl = document.querySelector(".weekdays");
-export const calendar = document.querySelector(".calendar");
-export const event = document.querySelector(".event");
-export const monthYearDisplay = document.querySelector(".monthYearDisplay");
-export const nextMonthBtn = document.querySelector(".nextMonthBtn");
-export const prevMonthBtn = document.querySelector(".prevMonthBtn");
